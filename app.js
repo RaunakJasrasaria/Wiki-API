b1dc54be31101447a5736fd78dfe38ed0f8dc9c3
raunak jasrasaria <raunakjasrasaria04@gmail.com>
2021-05-22 01:07:35 +0530

    get feature

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const ejs = requiree("ejs");
+const ejs = require("ejs");
 
 const app = express();
 app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true});
+app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB",{
   useNewUrlParser:true,
-  UseUnifiedTopology:true
+  useUnifiedTopology:true
 });
 
 const articleSchema = new mongoose.Schema({
@@ -18,6 +18,16 @@ const articleSchema = new mongoose.Schema({
 });
 
 const Article = mongoose.model("Article",articleSchema);
+
+app.get("/article",function(req,res){
+  Article.find(function(err,foundArticles){
+    if(err){
+      res.send(err);
+    }else{
+      res.send(foundArticles);
+    }
+  });
+});
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
