61258044e852ec1bf1eb6858b77360f27cc8b1df
raunak jasrasaria <raunakjasrasaria04@gmail.com>
2021-05-22 02:22:07 +0530

    post feature using postman

--- app.js
+++ app.js
@@ -29,6 +29,21 @@ app.get("/article",function(req,res){
   });
 });
 
+
+app.post("/article",function(req,res){
+  const newArticle = new Article({
+    title:req.body.title,
+    content:req.body.content
+  });
+  newArticle.save(function(err){
+    if(err){
+      res.send(err);
+    }else{
+      res.send("Successfully posted the data");
+    }
+  });
+});
+
 app.listen(3000,function(){
   console.log("Server started on port 3000");
 });
